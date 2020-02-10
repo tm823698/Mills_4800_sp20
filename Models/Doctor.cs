1caf089aac7da0ed4e9a6ffbcd25434b9e6bc10a
Thomas Mills <tm863215@ohio.edu>
2020-02-10 10:38:43 -0500

    save

---
+++ Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mills_4800.Models
+{
+    public class Doctor
+    {
+        public int doctorID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        
+
+
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mills_4800.Models
+{
+    public class Patient
+    {
+        public int patientID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime patientSince { get; set; }
+    }
+}
